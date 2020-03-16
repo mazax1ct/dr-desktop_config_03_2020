c6d76d02c3bc4547e2738adb491d3917968e99c5
mazax1ct <vova@giveandget.ru>
2020-03-16 18:23:11 +0300

    fuck this shit

--- build/js/smart-config.js
+++ build/js/smart-config.js
@@ -2,12 +2,7 @@
 if(window.location.hash.substring(1) == 'accessories') {
   $('.nc__section[data-hash="accessories"] .nc__inner').slideDown(0, function () { //открываем секцию аксессов
     $(".nc__inner").not($('.nc__section[data-hash="accessories"] .nc__inner')).slideUp(0, function () { //закрываем остальные секции
-      $(".nc__inner").not($('.nc__section[data-hash="accessories"] .nc__inner')).removeClass("is-active"); //закрываем внутренности остальных секций
-      //откручиваем страницу к заголовку открытой секции
-      var topOffset = $('.nc__section[data-hash="accessories"]').offset().top - $('.header').height();
-      $("html, body").animate({
-          scrollTop: topOffset
-      }, 0);
+      $(".nc__inner").not($('.nc__section[data-hash="accessories"] .nc__inner')).removeClass("is-active"); //закрываем внутренности остальных секций      
     });
 
     //переключаем класс активности текущей родительской секции
@@ -34,23 +29,9 @@ $(".js-config-result").click(function () {
   $('.nc__section').removeClass('is-active');
   $('.nc__section--result').addClass('is-active');
 
-  if($('body').width() < 992) { //отсекаем мобилы по ширине body 992px
-    $('.nc__inner').slideUp(0, function () { //закрываем секции
-      $('.nc__inner--result').slideDown(0, function () { //открываем итог
-        $('.nc__inner').removeClass("is-active"); //меням классы активности
-        $('.nc__inner--result').addClass("is-active");
-        //откручиваем к началу блока
-        var topOffset = $('.nc__inner--result .config-title').offset().top - $('.header').height();
-        $("html, body").animate({
-            scrollTop: topOffset
-        }, 0);
-      });
-    });
-  } else {
-    //меняем калссы активности
-    $('.nc__inner').removeClass("is-active");
-    $('.nc__inner--result').addClass("is-active");
-  }
+  //меняем классы активности
+  $('.nc__inner').removeClass("is-active");
+  $('.nc__inner--result').addClass("is-active");
 
   //переключаем кнопки в футере
   $('.footer__inner').removeClass('is-active');
@@ -66,95 +47,15 @@ $(".js-config-edit").click(function () {
     $('.nc__section').removeClass('is-active');
     $('.nc__section[data-hash="accessories"]').addClass('is-active');
 
-    if($('body').width() < 992) { //отсекаем мобилы по ширине body 992px
-      $('.nc__inner').slideUp(0, function () {
-        $('.nc__inner').removeClass("is-active"); //убираем классы активности
-        $('.nc__section[data-hash="accessories"] .nc__inner').slideDown(0, function () {
-          $('.nc__section[data-hash="accessories"] .nc__inner').addClass("is-active"); //вешаем класс активности на главную секцию
-          //откручиваем страницу к началу блока
-          var topOffset = $('.nc__section[data-hash="accessories"] .nc__inner').prev('.config-title').offset().top - $('.header').height();
-          $("html, body").animate({
-              scrollTop: topOffset
-          }, 0);
-        });
-      });
-    }
-  } else {
     //переключаем видимость основной секции на начало конфига
     $('.nc__section').removeClass('is-active');
     $('.nc__section:first').addClass('is-active');
-
-    if($('body').width() < 992) { //отсекаем мобилы по ширине body 992px
-      $('.nc__inner').slideUp(0, function () {
-        $('.nc__inner').removeClass("is-active"); //убираем классы активности
-        $('.nc__inner--config').slideDown(0, function () {
-          $('.nc__inner--config').addClass("is-active"); //вешаем класс активности на главную секцию
-          //откручиваем страницу к началу блока
-          var topOffset = $('.nc__inner--config').prev('.config-title').offset().top - $('.header').height();
-          $("html, body").animate({
-              scrollTop: topOffset
-          }, 0);
-        });
-      });
-    }
   }
 
   //переключаем кнопки в футере
   $('.footer__inner').removeClass('is-active');
   $('.footer__inner--main').addClass('is-active');
 
-  return false;
-});
-
-//аккордион секций конфига
-$(".js-config-section-opener").click(function () {
-  var section_title = $(this);
-  var section_id = $(this).attr('data-section');
-  var section_parent = $(this).parent('.nc__section');
-
-  $('.nc__inner[data-section=' + section_id + ']').slideDown(0, function () { //открываем секцию на заголовок которой нажали
-    $(".nc__inner").not($('.nc__inner[data-section=' + section_id + ']')).slideUp(0, function () { //закрываем остальные секции
-      $(".nc__inner").not($('.nc__inner[data-section=' + section_id + ']')).removeClass("is-active"); //закрываем внутренности остальных секций
-      //откручиваем страницу к заголовку открытой секции
-      var topOffset = section_title.offset().top - $('.header').height();
-      $("html, body").animate({
-          scrollTop: topOffset
-      }, 0);
-    });
-
-    //переключаем класс активности текущей родительской секции
-    $('.nc__section').removeClass("is-active");
-    section_parent.addClass("is-active");
-
-    //переключаем класс активности текущей секции
-    $('.nc__inner[data-section=' + section_id + ']').addClass("is-active");
-  });
-
-  //переключаем кнопки в футере
-  $('.footer__inner').removeClass('is-active');
-  $('.footer__inner--main').addClass('is-active');
-  return false;
-});
-
-//аккордион подсекций конфига
-$(".js-config-subsection-opener").click(function () {
-  var section_title = $(this);
-  var section_id = $(this).attr('data-section');
-  var parent_section_id = $(this).attr('data-parent-section');
-
-  $('.nc-c-list__item[data-section=' + section_id + ']').slideDown(0, function () {
-    $('.nc__inner[data-section=' + parent_section_id + '] .nc-c-list__item:not(.nc-c-list__item[data-section=' + section_id + '])').slideUp(0, function () {
-      $('.nc__inner[data-section=' + parent_section_id + '] .nc-c-list__item:not(.nc-c-list__item[data-section=' + section_id + '])').removeClass("is-active");
-      //откручиваем страницу к заголовку секции, 275 высота блока с графиками на мобилах
-      var topOffset = section_title.offset().top - 275 - $('.header').innerHeight();
-      $("html, body").animate({
-          scrollTop: topOffset
-      }, 0);
-    });
-
-    //переключаем класс активности текущей подсекции
-    $('.nc-c-list__item[data-section=' + section_id + ']').addClass("is-active");
-  });
   return false;
 });
 
